4eb019e55e1456c2adaf124f3329398d926932d9
Azor CeHa <arsyabernafas@gmail.com>
2025-12-18 20:44:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
-global.botname = 'Hitori Bot'
+global.owner = ['628811784465'] //['628','628'] 2 owner atau lebih
+global.author = 'Muhammad Arsyadin Awwal'
+global.botname = 'Toga Soft'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
 
@@ -29,16 +29,16 @@ global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum
 
 global.fake = {
 	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
-	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	thumbnailUrl: 'https://i.ibb.co.com/20D9JPJJ/toga.jpg',
+	thumbnail: fs.readFileSync('./src/media/toga.jpeg'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
 }
 
 global.my = {
-	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
-	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
+	yt: 'https://youtube.com/@azorceha',
+	gh: 'https://github.com/AzorCeHa',
+	gc: 'https://chat.whatsapp.com/JmEKycWxLBE1NmkHdIw2kl',
 	ch: '120363250409960161@newsletter',
 }
 
